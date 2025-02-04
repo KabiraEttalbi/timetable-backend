4b4b975544b301fbecce562a96276b4c57bd4c21
CoderOuma <oumaymarochdi01@gmail.com>
2025-02-04 22:38:35 +0100

    salles,modules,niveaux are ready to use

--- src/salles/salles.module.ts
+++ src/salles/salles.module.ts
@@ -1,9 +1,16 @@
-import { Module } from "@nestjs/common";
-import { SalleController } from './controllers/salle/salle.controller';
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Salle, SalleSchema } from './models/salle.model';
 import { SalleService } from './services/salle/salle.service';
+import { SalleController } from './controllers/salle/salle.controller';
+
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Salle.name, schema: SalleSchema }]), // Ajout du modèle
+  ],
+  providers: [SalleService],
   controllers: [SalleController],
-  providers: [SalleService]
+  exports: [SalleService], // Si utilisé dans un autre module
 })
 export class SallesModule {}
