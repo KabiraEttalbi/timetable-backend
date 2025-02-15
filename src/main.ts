4e7774d19de288be26deb9bb74a183c216811e46
Kabira Ettalbi <kabiraettalbi@gmail.com>
2025-02-15 03:42:13 +0100

    Add multer for file uploads and increase payload size limit to 10MB

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // Enable CORS
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 3001);
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ extended: true, limit: '10mb' }));
+
 }
-bootstrap();
+bootstrap();// Increase payload size limit (e.g., 10MB)
