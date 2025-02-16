2266c79e2be75908fcec4e4ea36ac45b420b7d98
Kabira Ettalbi <kabiraettalbi@gmail.com>
2025-02-16 20:58:40 +0100

    Fix import paths in Niveau controller and service; update e2e test module import

--- src/niveaux/controllers/niveau/niveau.controller.ts
+++ src/niveaux/controllers/niveau/niveau.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common'
 import { NiveauService } from '../../services/niveau/niveau.service';
 import { Niveau } from '../../models/niveau.model';
 import { CreateNiveauDto } from '../../dto/create-niveau.dto';
-import { UpdateNiveauDto } from '../..//dto/update-niveau.dto';
+import { UpdateNiveauDto } from '../../dto/update-niveau.dto';
 
 @Controller('niveau')
 export class NiveauController {
